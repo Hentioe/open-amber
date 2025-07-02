68d312035faa4832e94b822484dddbb3d8c065d5
Hentioe <meow.i5.br@gmail.com>
2025-07-02 12:03:11 +0800

    Migrate Capinde to new API

--- src/utils/capinde.ts
+++ src/utils/capinde.ts
@@ -1,5 +1,6 @@
 import config from "../config";
 import { strictify } from "../helpers";
+import log from "../log";
 
 export async function createLocal(params: Captcha.LocalGenParams) {
   let base_params = {};
@@ -16,11 +17,20 @@ export async function createLocal(params: Captcha.LocalGenParams) {
     };
   }
 
-  const resp = await (await fetch(`${config.CAPINDE_BASE_URL}/api/gen/local`, {
+  const resp = await fetch(`${config.CAPINDE_BASE_URL}/api/captchas?type=classic`, {
     method: "POST",
     body: JSON.stringify({ base_dir: "/out", base_params, ttl_secs: params.ttlSecs }),
     headers: { "Content-Type": "application/json" },
-  })).json();
+  });
 
-  return strictify<Captcha.LocalData>(resp);
+  if (resp.status !== 200) {
+    if (resp.status !== 500) {
+      const error = await resp.json() as { message: string };
+      log.error(`Failed to create CAPTCHA: ${error.message}, status: ${resp.status}`);
+    } else {
+      log.error(`Failed to create CAPTCHA, status: ${resp.status}`);
+    }
+  }
+
+  return strictify<Captcha.LocalData>(await resp.json());
 }
